975d243fa6ff2318c845a6820183e3b97a39b5df
jeffGS05 <49353053+jeffGS05@users.noreply.github.com>
2019-04-10 06:42:48 -0600

    Add files via upload

---
+++ Basicpython.py
@@ -0,0 +1,16 @@
+
+año = 2019
+saludo = ("Hola año" + str(año))
+print(saludo)
+#############
+
+nueva_lista=["hola", "a", "todos"]
+print(nueva_lista)
+
+###########
+
+pinumbre = float(0.314)
+print(pinumbre)
+
+
+
